e8ec2cf384d9251b57570ebdace680e32cdf5a5b
taraskaduk <taras.kdk@gmail.com>
2018-06-18 18:41:14 -0400

    RData

--- weather-data/weather-import.R
+++ weather-data/weather-import.R
@@ -5,23 +5,42 @@ library(tidyverse)
 url_cities <- 'https://simplemaps.com/static/data/us-cities/uscitiesv1.4.csv'
 
 cities <- read_csv(url_cities) %>% 
-  rename(latitude = lat,
-         longitude = lng)
+  rename(lon = lng)
 
 write_csv(cities, "data/cities.csv")
 
 # Stations -----------------------------------------------------------------
 
-stations <- read_table("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt", 
-                       skip = 20,
-                       na = c('999999', '99999', '-999.0', ''))
+url_stations <- "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt"
+stations_import <- read_table(url_stations,
+                              col_types = cols(BEGIN = col_date(format = "%Y%m%d"), 
+                                               CALL = col_character(), 
+                                               CTRY = col_character(), 
+                                               `ELEV(M)` = col_double(), 
+                                               END = col_date(format = "%Y%m%d"), 
+                                               LAT = col_double(), 
+                                               LON = col_double(), 
+                                               ST = col_character(), 
+                                               `STATION NAME` = col_character(), 
+                                               USAF = col_character(), 
+                                               WBAN = col_character()),
+                              skip = 20,
+                              na = c('-999.0', ''))
 
-colnames(stations) <- tolower(colnames(stations))
+colnames(stations_import) <- tolower(colnames(stations_import))
+
+stations <- stations_import %>% 
+  rename(station = `station name`,
+         elev = `elev(m)`) %>% 
+  filter(!is.na(usaf) & !(is.na(lat) | is.na(lon)))
 
 write_csv(stations, "data/stations.csv")
 
 
 # Data --------------------------------------------------------------------
+
+weather_data <- NULL
+
 for (year in 2014:2017) {
 
   file <- paste0('gsod_',year)
@@ -36,7 +55,7 @@ for (year in 2014:2017) {
                      read_table,
                      col_types = cols(.default = "c",
                                       YEARMODA = col_date(format = "%Y%m%d")),
-                     na = c("9999.9", '99999', '99.99', '999.9', '0.00I')
+                     na = c("9999.9", '99.99', '999.9', '0.00I')
                      )
 
   colnames(data_all) <- tolower(colnames(data_all))
@@ -53,27 +72,31 @@ for (year in 2014:2017) {
     # precipitation and snowfall NAs can be converted to 0 for this project
     # (see data description for details)
     replace_na(replace = list(prcp = 0, sndp = 0))
+  
+  weather_data <- rbind(weather_data, data_clean)
+
+  # data_us <- data_clean %>%
+  #   inner_join(stations_us, by = c('stn' = 'usaf'))
+  # 
+  # data_us %>%
+  #   select(lat, lon, stn) %>%
+  #   distinct() %>%
+  #   ggplot(aes(x=lon, y = lat)) + geom_point(size = 0.25, alpha = 0.1)
+  # 
+  # data_us %>%
+  #   mutate(latr = round(lat*4,0)/4,
+  #          lonr = round(lon*4,0)/4) %>%
+  #   select(latr, lonr, stn) %>%
+  #   distinct() %>%
+  #   ggplot(aes(x=lonr, y = latr)) + geom_point(size = 0.25, alpha = 0.8)
 
 
-  data_us <- data_clean %>%
-    inner_join(stations_us, by = c('stn' = 'usaf'))
-
-  data_us %>%
-    select(lat, lon, stn) %>%
-    distinct() %>%
-    ggplot(aes(x=lon, y = lat)) + geom_point(size = 0.25, alpha = 0.1)
-
-  data_us %>%
-    mutate(latr = round(lat*4,0)/4,
-           lonr = round(lon*4,0)/4) %>%
-    select(latr, lonr, stn) %>%
-    distinct() %>%
-    ggplot(aes(x=lonr, y = latr)) + geom_point(size = 0.25, alpha = 0.8)
-
-
-  write_csv(data_us, paste0("data/data_us_", year, ".csv"))
+  # write_csv(data_clean, paste0("data/data_clean_", year, ".csv"))
 
   file.remove(destfile)
   unlink(file, recursive = TRUE)
 
 }
+
+
+save(cities, stations, weather_data, file = "data/data.RData")
