40e1fed4f2a692cc8064de88377dd3dd08394c5a
Taras Kaduk <taraas.online@gmail.com>
2018-07-20 18:56:36 -0400

    Working knnreg model

--- weather/R/weather-3-predict.R
+++ weather/R/weather-3-predict.R
@@ -8,6 +8,7 @@ library(rsample)
 library(caret)
 library(recipes)
 library(yardstick)
+library(RANN)
 
 setwd("weather")
 
@@ -44,39 +45,120 @@ load("data/2-tidy.RData")
 
 set.seed(4595)
 
-data_select <- data_all %>% select(lat.0, lon.0, flag, yday, temp_mean, temp_max, temp_min, precip, snow)
+data_select <- data_all %>% select(lat.0, lon.0, flag, yday, year, temp_mean, temp_max, temp_min, precip, snow)
 
 data_missing <- data_select %>% filter(flag == "missing") %>% select(-flag)
 data_exist <- data_select %>% filter(flag == "existing") %>% select(-flag)
 
-data_split <- initial_split(data_exist, strata = "temp_mean")
-data_train <- training(data_split)
-data_test  <- testing(data_split)
-nrow(data_train)/nrow(data_exist)
+# data_split <- initial_split(data_exist, strata = "temp_mean")
+# data_train <- training(data_split)
+# data_test  <- testing(data_split)
+# nrow(data_train)/nrow(data_exist)
+# 
+# data_sample <- data_select %>% 
+#   select(-flag) %>% 
+#   arrange(lat.0, lon.0, yday) %>% 
+#   head(15000)
+
+
+
+
+
+# LM… This is shiiiit!!! --------------------------------------------------
+
+# mod_lm <- lm(temp_mean ~ lat.0 + lon.0 + yday, data = data_train)
+# 
+# pred_data <- data_test %>% 
+#   mutate(pred = predict(mod_lm, newdata = .))
+# 
+# summary(mod_lm)
+
+
+# Find nearest neighbors before doing knn??? ------------------------------
+
+data_knn_miss <- data_missing %>% 
+  select(lat.0, lon.0, yday, year)
+
+data_knn_exist <- data_exist %>%
+  select(lat.0, lon.0, yday, year)
+
+nn <- nn2(data = data_knn_exist,
+          query = data_knn_miss,
+          k = 10)
+
+# nn[[1]]
+# 
+# data_knn_miss[1,]
+# data_knn_exist[14028,]
+# data_knn_exist[14586,]
+# data_knn_exist[14029,]
+# data_knn_exist[15315,]
+# data_knn_exist[16410,]
+# 
+# class(nn[[1]])
+
+
+index_leave <- nn[[1]] %>% as.vector() %>% unique()
+
+data_leave <- data_exist[index_leave, ]
+
+
+
 
 
 # KNN ---------------------------------------------------------------------
 
-## Recipes 
+## Recipes.... NOPE
+
+# data_join <- data_leave %>% 
+#   union_all(data_missing)
+
+# mod_rec <- recipe(~., data = data_join) %>% 
+#   add_role(temp_mean, temp_max, temp_min, precip, snow,
+#            new_role = "outcome") %>% 
+#   add_role(lat.0, lon.0, yday, new_role = "predictor") %>% 
+  # step_scale(all_predictors()) %>% 
+  # step_center(all_predictors()) %>% 
+  #step_knnimpute(all_numeric(), -all_predictors())
+
+#mod_prep <- prep(mod_rec, training = data_, retain = TRUE)
 
 
-mod_rec <- recipe( ~., data = data_train) %>% 
-  add_role(temp_mean, temp_max, temp_min, precip, snow,
-           new_role = "outcome") %>% 
-  add_role(lat.0, lon.0, yday, new_role = "predictor") %>% 
-  step_scale(all_predictors()) %>% 
-  step_center(all_predictors())
 
-mod_rec_trained <- prep(mod_rec, training = data_train, retain = TRUE, verbose = TRUE)
+f_knn <- function(df_train = data_leave, df_pred = data_missing, col){
+  
+  formula <- as.formula(substitute(col ~ lat.0 + lon.0 + yday + year, list(col = as.name(col))))
+  
+  knn_train_mod <- knnreg(formula = formula, 
+                          data = df_train,
+                          k = 5)
+  
+  col <- quo_name(enquo(col))
+  
+  
+  predict(knn_train_mod, newdata = df_pred)
+  
+  # pred_data <- df_pred %>% 
+  #   head(100) %>% 
+  #   mutate(!!col := predict(knn_train_mod, newdata = .))
+  
+}
 
-data_train_prep <- bake(mod_rec_trained,newdata = data_train)
-data_test_prep <- bake(mod_rec_trained,newdata = data_test)
-data_pred_prep <- bake(mod_rec_trained,newdata = data_missing)
+
+test <- f_knn(data_leave, data_missing %>% head(100), col = "temp_mean")
 
 
-knn_train_mod <- knnreg(temp_mean ~ lat.0 + lon.0 + yday, 
-                        data = data_train_prep,
-                        k = 5)
+test <- data_missing %>% head(100) %>% 
+  mutate(temp_mean = f_knn(df_pred = ., col = "temp_mean"),
+         temp_min =  f_knn(df_pred = ., col = "temp_min"),
+         temp_max =  f_knn(df_pred = ., col = "temp_max"),
+         precip =    f_knn(df_pred = ., col = "precip"),
+         snow =      f_knn(df_pred = ., col = "snow"))
+  
+  
 
 
-  predict(knn_train_mod, newdata = data_test_prep %>% head(100))
+## -------------
+
+
+
